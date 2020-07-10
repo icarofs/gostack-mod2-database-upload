9f30e95a69f45897b7c8bedc5e1eb3760bc5099b
Ícaro Ferreira de Souza <icaro.souza@ntendencia.com.br>
2020-07-10 16:19:00 -0300

    WIP - create category

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,7 +1,8 @@
 // import AppError from '../errors/AppError';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface Request {
   title: string;
@@ -18,8 +19,28 @@ class CreateTransactionService {
     category,
   }: Request): Promise<Transaction> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
 
-    const transaction = transactionsRepository.create({ title, value, type });
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
 
     await transactionsRepository.save(transaction);
 
